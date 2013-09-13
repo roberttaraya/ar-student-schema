9f8604e16c0c0e4cc94dbd06ecb63eb1f09e67c9
Robert Taraya <robert.taraya@gmail.com>
2013-09-12 17:32:48 -0700

    create Student class with name method to retrieve a students fill name, and age method to calculate the age of a student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,11 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    Time.now.year-birthday.year
+  end
 end
