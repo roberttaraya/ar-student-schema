45bc9d0c54fa24f8ac43623f89acc5ed9b9961fc
Robert Taraya <robert.taraya@gmail.com>
2013-09-12 18:25:13 -0700

    add another validation for phone number. validates number of digits at least 10 or else raises an error

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,6 +5,10 @@ validates :email, format: { with: /.+@.+\.\w{2,}/}, uniqueness: true
 validates :age, numericality: { greater_than: 5 }
 validates :phone, length: { minimum: 12 }
 
+  def phone_number_must_contain_at_least_10_digits
+     raise "Check phone number: wrong number of digits" if phone.gsub(/\D/,"").length < 10
+  end
+
   def name
     "#{first_name} #{last_name}"
   end
