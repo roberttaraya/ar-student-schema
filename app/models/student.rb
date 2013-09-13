31efb7b632aa7e93021fecbfc189ba6c6d98bbca
Robert Taraya <robert.taraya@gmail.com>
2013-09-12 18:12:25 -0700

    validates email format, email uniqueness, min student age, min phone digits

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,10 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
+validates :email, format: { with: /.+@.+\.\w{2,}/}, uniqueness: true
+validates :age, numericality: { greater_than: 5 }
+validates :phone, length: { minimum: 12 }
+
   def name
     "#{first_name} #{last_name}"
   end
@@ -9,3 +13,4 @@ class Student < ActiveRecord::Base
     Time.now.year-birthday.year
   end
 end
+
